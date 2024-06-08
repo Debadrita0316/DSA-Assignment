9a458a5dfc0e4c4943673055aebbac5597e3fb0f
Debadrita Konar <debadritakonar2005@gmail.com>
2024-06-08 21:34:53 +0530

    updated

--- DSA Assignment 4/SingleLinkedList.java
+++ DSA Assignment 4/SingleLinkedList.java
@@ -164,428 +164,228 @@ public class SingleLinkedList {
 
     }
 
-
-
     public static Node DelBeg(Node start) 
-
     {
-
         if (start == null) {
-
             System.out.println("Empty LinkedList");
-
         } else {
-
             start = start.next;
-
             System.out.println("Node Deleted");
-
         }
-
         return start;
-
     }
-
-
 
     public static Node DelEnd(Node start) 
-
     {
-
         if (start == null) {
-
             System.out.println("Empty LinkedList");
-
         } else if (start.next == null) { // if there's only one element
-
             start = null;
-
             System.out.println("Last Node deleted");
-
         } else {
-
             Node p = start;
-
             Node q = null;
-
             while (p.next != null) {
-
                 q = p;
-
                 p = p.next;
-
             }
-
             q.next = null;
-
             System.out.println("Last Node deleted");
-
         }
-
         return start;
-
     }
 
-
-
     public static Node DelAny(Node start) 
-
     {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter position you want to delete: ");
-
         int pos = sc.nextInt();
 
-
-
         if (start == null) {
-
             System.out.println("Empty LinkedList");
-
         } else if (pos == 1) {
-
             start = start.next;
-
             System.out.println("Node is deleted");
-
         } else {
-
             Node p = start;
-
             for (int i = 1; i < pos - 1 && p != null; i++) {
-
                 p = p.next;
-
             }
 
             if (p != null && p.next != null) {
-
                 p.next = p.next.next;
-
                 System.out.println("Node is deleted");
-
             } else {
-
                 System.out.println("Position out of range");
-
             }
-
         }
-
         return start;
-
     }
 
-
-
     public static void search(Node start) 
-
     {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter Registration No: ");
-
         int regNo = sc.nextInt();
 
-
-
         if (start == null) {
-
             System.out.println("Empty linkedList");
-
         } else {
-
             Node p = start;
-
             boolean found = false;
-
             while (p != null) {
-
                 if (p.regd_no == regNo) {
-
                     System.out.println("Registration Found, Enter the updated Marks: ");
-
                     p.mark = sc.nextFloat();
-
                     found = true;
-
                     break;
-
                 }
-
                 p = p.next;
-
             }
 
             if (found) {
-
                 System.out.println("Marks updated!");
-
             } else {
-
                 System.out.println("Registration number not found.");
-
             }
-
         }
-
     }
-
-
 
     public static int count(Node start) 
-
     {
-
         int c = 0;
-
         Node p = start;
-
         while (p != null) {
-
             c++;
-
             p = p.next;
-
         }
-
         return c;
-
     }
-
-
 
     public static Node reverse(Node start) 
-
     {
-
         Node prev = null, current = start, next = null;
-
-
-
         while (current != null) {
-
             next = current.next;
-
             current.next = prev;
-
             prev = current;
-
             current = next;
-
         }
-
-
 
         start = prev;
-
         System.out.println("Linked list reversed.");
-
         return start;
-
     }
-
-
 
     public static void sort(Node start) 
-
     {
-
         if (start == null) {
-
             System.out.println("Empty linked list");
-
             return;
-
         }
-
-
 
         Node m = start;
-
         while (m.next != null) {
-
             Node q = start;
-
             while (q.next != null) {
-
                 Node p = q.next;
-
                 if (q.mark > p.mark) {
-
                     // Swap registration numbers
-
                     int reg = q.regd_no;
-
                     q.regd_no = p.regd_no;
-
                     p.regd_no = reg;
-
-
-
                     // Swap marks
-
                     float mark = q.mark;
-
                     q.mark = p.mark;
-
                     p.mark = mark;
-
                 }
-
                 q = q.next;
-
             }
-
             m = m.next;
-
         }
-
         System.out.println("Linked list sorted based on marks.");
-
     }
-
 
 
     public static void main(String[] args) 
-
     {
-
         Scanner sc = new Scanner(System.in);
-
         int choice;
-
         Node start = null;
-
-
-
         while (true) {
-
             System.out.println("\nMenu:");
-
             System.out.println("0: Exit");
-
             System.out.println("1. Create");
-
             System.out.println("2. Display");
-
             System.out.println("3. Insert at Beginning");
-
             System.out.println("4. Insert at End");
-
             System.out.println("5. Insert at Any Position");
-
             System.out.println("6. Delete from Beginning");
-
             System.out.println("7. Delete from End");
-
             System.out.println("8. Delete from Any Position");
-
             System.out.println("9. Search and Update by Registration Number");
-
             System.out.println("10. Count Nodes in the Linked List");
-
             System.out.println("11. Sort the Linked List based on Marks");
-
             System.out.println("12. Reverse the Linked List");
-
             System.out.println("Enter your choice:");
-
             choice = sc.nextInt();
 
-
-
             switch (choice) {
-
                 case 0:
-
                     System.exit(0);
-
                     break;
 
                 case 1:
-
                     start = create(start);
-
                     break;
 
                 case 2:
-
                     display(start);
-
                     break;
 
                 case 3:
-
                     start = InsBeg(start);
-
                     break;
 
                 case 4:
-
                     start = InsEnd(start);
-
                     break;
 
                 case 5:
-
                     start = InsAny(start);
-
                     break;
 
                 case 6:
-
                     start = DelBeg(start);
-
                     break;
 
                 case 7:
-
                     start = DelEnd(start);
-
                     break;
 
                 case 8:
-
                     start = DelAny(start);
-
                     break;
 
                 case 9:
-
                     search(start);
-
                     break;
 
                 case 10:
-
                     System.out.println("Number of nodes: " + count(start));
-
                     break;
 
                 case 11:
-
                     sort(start);
-
                     break;
 
                 case 12:
-
                     start = reverse(start);
-
                     break;
 
                 default:
-
                     System.out.println("Wrong Choice");
-
             }
-
         }
-
     }
-
 }
